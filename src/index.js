1020a6459729437c10e05a33f51bea681fc36121
Teoslip <tjachiet@gmail.com>
2025-07-09 12:22:19 +0200

    end result hardcoded in html css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './reset.css';
+import './style.css';
 
-console.log('hello');
+const container = document.querySelector('.container');
+const title = document.querySelector('.title');
+const list = document.querySelector('ul');
+const listElements = [...document.querySelectorAll('li')];
+
+container.addEventListener('mouseenter', () => {
+    list.classList.add('visible');
+});
+
+container.addEventListener('mouseleave', () => {
+    list.classList.remove('visible');
+})
